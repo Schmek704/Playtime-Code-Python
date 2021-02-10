6a6795d9a794db56618a6193f9918479ab252779
Dan McKeon <78289818+Schmek704@users.noreply.github.com>
2021-02-09 20:28:06 -0500

    Update street_craps.py

--- street_craps.py
+++ street_craps.py
@@ -1,7 +1,9 @@
 #  Street Dice Game
-#  Roll a 7 to win coming out
+#  Roll a 7 or 11 to win coming out
+#  Roll 2,3, or 12 coming out and lose craps
 #  Any other number and you have to roll that same number again before you roll a 7
 #  Simplified Craps Game
+#  No wagering yet
 
 import random
 
@@ -18,31 +20,39 @@ throw1 = Dice()
 point = 0
 result = []
 total = 0
+game = ""
 
-
-while rolling:
-    print(input("\n\nHit Enter to Roll them bones daddy >>>"))
-    result = throw1.roll()
-    total = (result[0] + result[1])
-    print(result)
-    if coming_out:
-        if total == 7:
-            print("Front Line Winner, pay the Line!")
-        else:
-            point = total
-            print(f"{point} is the point!  Come on shooter, let's roll another {point}!!!")
-            coming_out = False
-    else:
-        if total == 7:
-            print("7 OUT!  Pay the dont's, take the line, pass the dice!!")
-            rolling = False
-        else:
-            if total == point:
-                print(f"{point} winner winner chicken dinner, pay the line, take the don't, the man is on fire!!!")
-                coming_out = True
+while game != "x":
+    while rolling:
+        print(input("\n\nHit Enter to Roll>>>"))
+        result = throw1.roll()
+        total = (result[0] + result[1])
+        print(result)
+        if coming_out:
+            if total == 7 or total == 11:
+                print("Front Line Winner, pay the Line!")
+            elif total == 2 or total == 3 or total == 12:
+                print("CRAPS!! Take the line. No fun on the Craps Train... heeeee's coming out again!!")
             else:
-                print(f"Keep Shooting my man, let's find that {point}, these people wanna get paid")
-print("Good Roll buddy, better luck next time...")
+                point = total
+                print(f"{point} is the point!  Come on shooter, let's roll another {point}!!!")
+                coming_out = False
+        else:
+            if total == 7:
+                print("7 OUT!  Pay the dont's, take the line, pass the dice!!")
+                rolling = False
+            else:
+                if total == point:
+                    print(f"{point} winner winner chicken dinner, pay the line, take the don't, the man is on fire!!!")
+                    coming_out = True
+                else:
+                    print(f"Keep Shooting my man, let's find that {point}, these people wanna get paid")
+    print("\n\nGood Roll buddy, better luck next time...")
+    rolling = True
+    coming_out = True
+    game = input('\n\nNext Shooter>> "Enter" for yes, "x" for Exit:  ')
+
+        
         
 
 
