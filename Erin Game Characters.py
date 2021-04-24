5924a187fe66619ac5c1aced8aacc5c73118e6ec
Dan McKeon <78289818+Schmek704@users.noreply.github.com>
2021-04-24 13:46:07 -0400

    Update Erin Game Characters.py

--- Erin Game Characters.py
+++ Erin Game Characters.py
@@ -1,7 +1,8 @@
 # This is an interactive meet & greet with characters that E7 is creating to implement in her game
 
 char_story = {
-'pearl' : "Meet Pearl! Pearl is a Fuzzball. She lives in Teeny Tinyville. \nShe has a large family, is the oldest of the children along with her brothers Freaky, Cover, and Chippy, and her sisters Squeaks and Dottie. \nHer mom, Cotton, is the sweetest snake in Teeny Tinyville, and loves all her children. \nPearl goes to Gingerbread Elementary, and is currently in 1st grade with her teacher Ms M. \nPearl loves to wear bows in her hair everywhere she goes! \nShe lives in a house with her sister Squeaks."
+'pearl' : "Meet Pearl! Pearl is a Fuzzball. She lives in Teeny Tinyville. \nShe has a large family, is the oldest of the children along with her brothers Freaky, Cover, and Chippy, and her sisters Squeaks and Dottie. \nHer mom, Cotton, is the sweetest snake in Teeny Tinyville, and loves all her children. \nPearl goes to Gingerbread Elementary, and is currently in 1st grade with her teacher Ms M. \nPearl loves to wear bows in her hair everywhere she goes! \nShe lives in a house with her sister Squeaks.",
+'stargazer' : "Meet Stargazer! Stargazer is a Unicorn. She lives on Hatch Island. \nShe has a VERY large family, is the 2nd youngest of the children along with her brothers Gill, Syd, and Zebrey,and her sisters Boa, Phoebe, Little Bubbles, Dorothy, Angel, and Leely. \nHer mom, Mal, who just happens to be a human, loves all her children. \nStargazer goes to Hatch Elementary, and is currently in Pre-K with her teacher Ms Philla. \nStargazer loves to looks at the stars at night! She is fascinated with comets and shooting stars. \nShe lives in a castle on Hatch Island."    
 }
 intros = True
 
